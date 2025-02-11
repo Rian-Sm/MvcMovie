05e9a254649cd0f8f12e1cade1356fa9ef76087d
Rian Macedo <riansm100@gmail.com>
2025-02-11 08:09:27 -0300

    🏗️Add project infra

---
+++ MVCMovie.Infra/Data/MovieContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Domain.Models;
+
+namespace MvcMovie.Infra.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().Property(i => i.Price).HasPrecision(18,2);
+        }
+    }
+}
